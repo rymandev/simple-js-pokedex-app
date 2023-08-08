5a3b6e32a33342852a512c03e25b75616476d3d0
Ryn <ryn@ryman.dev>
2023-08-07 22:56:11 -0700

    Update scripts.js
    Improved formatting of pokemonList array for readability, and added more Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,77 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}
-    {name: 'Charmander', height: 0.6, type: ['fire']}
-    {name: 'Squirtle', height: 0.5, type: ['water']}
-    {name: 'Caterpie', height: 0.3, type: ['bug']}
-    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']}
+    {
+        name: 'Bulbasaur', 
+        height: 0.7, 
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur', 
+        height: 1, 
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaurr', 
+        height: 2, 
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander', 
+        height: 0.6, 
+        type: ['fire']
+    },
+    {
+        name: 'Charmeleon', 
+        height: 1.1, 
+        type: ['fire']
+    },
+    {
+        name: 'Charizard', 
+        height: 1.7, 
+        type: ['fire', 'flying']
+    },
+    {
+        name: 'Squirtle', 
+        height: 0.5, 
+        type: ['water']
+    },
+    {
+        name: 'Wartortle', 
+        height: 1, 
+        type: ['water']
+    },
+    {
+        name: 'Blastoise', 
+        height: 1.6, 
+        type: ['water']
+    },
+    {
+        name: 'Caterpie', 
+        height: 0.3, 
+        type: ['bug']
+    },
+    {
+        name: 'Metapod', 
+        height: 0.7, 
+        type: ['bug']
+    },
+    {
+        name: 'Butterfree', 
+        height: 1.1, 
+        type: ['bug', 'flying']
+    },
+    {
+        name: 'Weedle', 
+        height: 0.3, 
+        type: ['bug', 'poison']
+    },
+    {
+        name: 'Kakuna', 
+        height: 0.6, 
+        type: ['bug', 'poison']
+    },
+    {
+        name: 'Beedrill', 
+        height: 1, 
+        type: ['bug', 'poison']
+    }
 ]
